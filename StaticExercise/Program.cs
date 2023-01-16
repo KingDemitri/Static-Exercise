a197cad35e8a51cd443dafad1c7ee42124162756
Demitri Cameron <oblivion9794@gmail.com>
2023-01-16 14:06:20 -0500

    initial commit that I should've made weeks ago

--- StaticExercise/Program.cs
+++ StaticExercise/Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+           //var tempInput = Console.ReadLine();
+           //FInd a way to use tempInput vs "36" below
+
+           var celsius  =   TempConverter.CelsiusToFahrenheit(36);
+           var fahrenheit = TempConverter.FahrenheitToCelsius((celsius * 9) / 5 + 32);
+
+            Console.WriteLine($"Celsius Temp Converted to Fahenrenheit: {fahrenheit}");
+            Console.WriteLine($"Original Temp (Celsius): {celsius}");
         }
     }
 }
+
+//Create a static class called TempConverter.
+
+//The class should have at least 2 methods, one called FahrenheitToCelsius that will require a double as a parameter and return a double, the other CelsiusToFahrenheit which will also require a double as a parameter and return a double.
+
+//Fill out these methods and call them in your Program.cs file. Make sure they’re accurate!
+
+//For reference: https://github.com/mvdoyle/StaticExercise
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticExercise
+{
+    internal class TempConverter
+    {
+        public static double FahrenheitToCelsius(double celsius)
+        {
+            return celsius; 
+        }
+
+        public static double CelsiusToFahrenheit(double fahrenheit)
+        {
+            return fahrenheit;
+        }
+    }
+}
